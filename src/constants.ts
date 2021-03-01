7717dbe63478bc6a3b08cccac8cbf8b7ea58e531
pankajjaiswal <pankajjaiswal@urbanclap.com>
2021-03-01 08:57:28 +0530

    Pankaj/ Parking Lot

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const DEBUG_MODE = `server running at port: 5000 ...
-1. Create
-2. Park
-3. Leave
-4. Find Slots for a age group
-5. Find Slot number for a car
-6. Find Registration Numbers for a age group`;;
---
+++ src/test/service/index.test.js
@@ -0,0 +1,14 @@
+
+// jest.mock("../../src/service/slots", () => ({
+//     fetchAll: jest.fn()
+// }));
+//
+// import {} from "../../src/service/slots";
+
+// function sum () {
+//     return 'pankaj';
+// }
+//
+// test("add", () => {
+//     expect(sum()).toBe('pankaj'))
+// });
